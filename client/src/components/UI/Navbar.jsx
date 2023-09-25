cbcd6e7d1a5f29db06896f86ace6943493b20b7c
aman4uas <aman4u.as@gmail.com>
2023-09-25 22:32:23 +0530

    update

--- client/src/components/UI/Navbar.jsx
+++ client/src/components/UI/Navbar.jsx
@@ -8,27 +8,57 @@ const Navbar = () => {
     setWindowLocation(window.location.hash);
   })
   return (
-    <div className='fixed z-10 top-0 w-full flex flex-row justify-between text-white bg-gradient h-[55px]' >
-
-      <div className='ml-[10vw] flex flex-row justify-center items-center'>
-        <Link className="flex flex-row justify-center items-center space-x-4" to="/">
-          <img src="assets/icon-white.png" alt="logo" className='h-[40px]' />
-          <h1 className='font-semibold font-sans text-2xl'>{COMPANY_NAME}</h1>
+    <div className="fixed z-10 top-0 w-full flex flex-row justify-between text-white bg-gradient h-[55px]">
+      <div className="ml-[10vw] flex flex-row justify-center items-center">
+        <Link
+          className="flex flex-row justify-center items-center space-x-4"
+          to="/"
+        >
+          <img src="assets/icon-white.png" alt="logo" className="h-[40px]" />
+          <h1 className="font-semibold font-sans text-2xl">{COMPANY_NAME}</h1>
         </Link>
       </div>
 
-      <div className='flex flex-row justify-center items-center space-x-[6vw] text-lg'>
-        <div className={`hover:drop-shadow-lg ` + (currentWindowLocation === "" ? 'border rounded-md p-1 font-semibold' : '')}><Link to="/">Home</Link></div>
-        <div className={`hover:drop-shadow-lg ` + (currentWindowLocation === "#About" ? 'border rounded-md p-1 font-semibold' : '')}><Link to="/#About">About</Link></div>
-        <div className={`hover:drop-shadow-lg ` + (currentWindowLocation === "#Contact" ? 'border rounded-md p-1 font-semibold' : '')}><Link to="#Contact">Contact</Link></div>
+      <div className="flex flex-row justify-center items-center space-x-[6vw] text-lg">
+        <div
+          className={
+            `hover:drop-shadow-lg ` +
+            (currentWindowLocation === ""
+              ? "border rounded-md p-1 font-semibold"
+              : "")
+          }
+        >
+          <Link to="/">Home</Link>
+        </div>
+        <div
+          className={
+            `hover:drop-shadow-lg ` +
+            (currentWindowLocation === "#About"
+              ? "border rounded-md p-1 font-semibold"
+              : "")
+          }
+        >
+          <Link to="/#About">About</Link>
+        </div>
+        <div
+          className={
+            `hover:drop-shadow-lg ` +
+            (currentWindowLocation === "#Contact"
+              ? "border rounded-md p-1 font-semibold"
+              : "")
+          }
+        >
+          <Link to="/server">Server</Link>
+        </div>
       </div>
 
-      <div className='mr-[10vw] flex flex-row justify-center items-center text-lg'>
-        <div className='py-1 px-4 bg-white border rounded text-[#FD6B00] font-semibold hover:drop-shadow-lg'><Link to="/login">Login</Link></div>
+      <div className="mr-[10vw] flex flex-row justify-center items-center text-lg">
+        <div className="py-1 px-4 bg-white border rounded text-[#FD6B00] font-semibold hover:drop-shadow-lg">
+          <Link to="/login">Login</Link>
+        </div>
       </div>
-
     </div>
-  )
+  );
 }
 
 export default Navbar
